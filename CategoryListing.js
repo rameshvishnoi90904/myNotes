eccd3b05334992159aa68c843e47c2b5a4049d52
Ramesh Vishnoi <rameshvishnoi90904@gmail.com>
2021-06-02 23:14:14 +0530

    first commit

---
+++ CategoryListing.js
@@ -0,0 +1,117 @@
+
+import React, {useContext, useEffect, useState} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  TouchableOpacity
+} from 'react-native';
+
+import {AuthContext} from './context/Context';
+import { WebView } from 'react-native-webview';
+import {RichEditor} from 'react-native-pell-rich-editor';
+
+function CategoryListingScreen({navigation, route}) {
+	const [state, update] = useContext(AuthContext);
+	const category = route.params.category;
+	const cData = state.categories.find((cItem) => {
+		return (cItem.name == category);
+	});
+
+	React.useLayoutEffect(() => {
+		navigation.setOptions({
+		  title: ''
+		});
+	  }, [navigation]);
+
+
+	const navigateToNotes = (nData) => {
+		navigation.navigate("Note",{
+			category:category,
+			noteData: nData
+		});
+	}
+
+	const openAddNotes = () => {
+		navigation.navigate("MyModal")
+	}
+
+	let notesPreview = [];
+	if (cData) {
+		notesPreview = cData.notes.map((nItem) => {
+			return (
+				<TouchableOpacity key={nItem.id} style={[styles.noteItem, styles.shadow]} onPress={() => {navigateToNotes(nItem)}}>
+					{
+						nItem.lastUpdateOn &&
+						<Text style={styles.timeStampText}>{new Date(nItem.lastUpdateOn).toDateString()}</Text>
+					}
+					<RichEditor
+						disabled={true}
+						initialContentHTML={nItem.data}
+					/>
+				</TouchableOpacity>
+			)
+		})
+	}
+	return (
+		<View style={styles.container}>
+			{notesPreview}
+			<TouchableOpacity style={styles.floatingAdd} onPress={openAddNotes}>
+				<Text style={styles.addText}>+</Text>
+			</TouchableOpacity>
+		</View>
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		paddingHorizontal: 20,
+		paddingTop: 10,
+		backgroundColor: 'white'
+	},
+	noteItem: {
+		backgroundColor: 'white',
+		marginBottom: 20, 
+		paddingVertical: 10,
+		borderRadius: 10 ,
+		// minHeight: 200,
+	},
+	timeStampText: {
+		color: 'red',
+		fontWeight: 'bold',
+		paddingHorizontal: 10
+	},
+	shadow: {
+		shadowColor: "#000",
+		shadowOffset: {
+			width: 0,
+			height: 2,
+		},
+		shadowOpacity: 0.23,
+		shadowRadius: 2.62,
+		elevation: 4,
+	},
+	addText: {
+		fontSize: 52,
+		color: 'white'
+	},
+	floatingAdd: {
+		position: 'absolute',
+		bottom: 50,
+		right: 20,
+		width: 70,
+		height: 70,
+		borderRadius: 35,
+		backgroundColor: 'red',
+		alignItems: 'center',
+		justifyContent: 'center'
+
+	},
+})
+
+export default CategoryListingScreen 
---
+++ LoginScreen.js
@@ -0,0 +1,113 @@
+import React, {useContext, useState, useRef, useEffect} from 'react';
+import {
+  SafeAreaView,
+  ScrollView,
+  StatusBar,
+  StyleSheet,
+  Text,
+  useColorScheme,
+  View,
+  Button,
+  ActivityIndicator,
+  Dimensions,
+  TouchableOpacity
+} from 'react-native';
+import {AuthContext} from './context/Context';
+
+
+// import auth from '@react-native-firebase/auth';
+const {width, height} = Dimensions.get("window");
+
+function LoginScreen({navigation, route}) {
+	const [state, update] = useContext(AuthContext);
+
+	console.log("state ", state);
+	const signinAnon = async () => {
+		update({
+			type: "LOGIN"
+		});
+		return;
+		auth()
+		.signInAnonymously()
+		.then(() => {
+			console.log('User signed in anonymously');
+		}).catch(error => {
+			if (error.code === 'auth/operation-not-allowed') {
+				console.log('Enable anonymous in your firebase console.');
+			}
+
+			console.error(error);
+		});
+	}
+
+	const googleSignIn = async () => {
+
+		update({
+			type: "LOGIN"
+		});
+		return;
+		const { idToken } = await GoogleSignin.signIn();
+
+		// Create a Google credential with the token
+		const googleCredential = auth.GoogleAuthProvider.credential(idToken);
+
+		// Sign-in the user with the credential
+		await auth().signInWithCredential(googleCredential);
+
+	}
+	return (
+		<SafeAreaView style={styles.container}>
+			<View style={styles.appHeader}>
+				<Text style={[styles.appText,{color: 'red'}]}>My</Text>
+				<Text style={styles.appText}> Notes</Text>
+			</View>
+			<View style={styles.optionContainer}>
+				<Text>Login here using</Text>
+				<TouchableOpacity style={styles.anonymouslyButton} onPress={signinAnon}>
+					<Text style={styles.anonymouslyText}>Anonymously</Text>
+				</TouchableOpacity>
+				<TouchableOpacity style={styles.anonymouslyButton} onPress={googleSignIn}>
+					<Text style={styles.anonymouslyText}>Google</Text>
+				</TouchableOpacity>
+			</View>
+		</SafeAreaView>	
+	);
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: 'white',
+		alignItems: 'center',
+	},
+	appHeader: {
+		justifyContent: 'center',
+		paddingVertical: 50,
+		flexDirection: 'row',
+		flex: 1,
+		alignItems: 'center'
+	},
+	appText: {
+		fontSize: 50,
+		fontWeight: 'bold',
+		color: '#1A237E'
+	},
+	optionContainer: {
+		flex: 2,
+	},
+	anonymouslyButton: {
+		backgroundColor: '#424242',
+		paddingHorizontal: 30,
+		paddingVertical: 10,
+		marginVertical: 20,
+		alignItems: 'center',
+		borderRadius: 10
+	},
+	anonymouslyText: {
+		color: 'white',
+		fontSize: 20,
+		fontWeight: 'bold'
+	}
+})
+
+export default LoginScreen; 
